3ce77c08121711fd9814ae87857fbdb4d9b3b95b
miguelr215 <drexelramos@gmail.com>
2025-07-12 14:14:55 -0400

    users

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -7,4 +11,6 @@ import { Component } from '@angular/core';
   templateUrl: './user.html',
   styleUrl: './user.css',
 })
-export class User {}
+export class User {
+  selectedUser = DUMMY_USERS[randomIndex];
+}
