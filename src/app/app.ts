52e65fa68f352cd593699f048ca0e96eb3349af3
miguelr215 <drexelramos@gmail.com>
2025-07-12 06:06:50 -0400

    user component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { User } from './user/user';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [HeaderComponent, User],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
---
+++ src/app/user/user.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.html',
+  styleUrl: './user.css',
+})
+export class User {}
